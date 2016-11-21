54ce0b4fcbcf05cd4a547d111f92da12d41499f6
layumi <12307130083@fudan.edu.cn>
2016-11-21 20:15:31 +0800

    1.1

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 # Center_Loss
 Idea is from [A Discriminative Feature Learning Approach for Deep Face Recognition ECCV16](http://ydwen.github.io/papers/WenECCV16.pdf)
+
 The author's code can be find in following links:
-[caffe](https://github.com/kpzhang93/caffe-face)
-[mxnet](https://github.com/pangyupo/mxnet_center_loss)
+
+*[caffe](https://github.com/kpzhang93/caffe-face);
+
+*[mxnet](https://github.com/pangyupo/mxnet_center_loss)
 
 
 I rebuild the code based on Matconvnet.
-![](https://github.com/layumi)
+![](https://github.com/layumi/2016_Center_Loss/blob/master/demo.jpg)
 
-#Usage
-To test:
+# How to train & test
+1.You may compile matconvnet first by running `gpu_compile.m`  (you need to change some setting in it)
 
-1.compile matconvnet.
+For more compile information, you can learn it from www.vlfeat.org/matconvnet/install/#compiling
 
-2.run 'show.m' to have fun~
+2.run `show.m` for test result.
 
-To train:
+3.If you want to train it by yourself, I have include the mnist data in the `data/imdb.mat`. (I have substract the mean of the data.) 
 
-1.compile matconvnet.
+4.Use `train_id_net_mnist.m` to have fun~
 
-2.run 'train_id_net_minist' to have fun~
+# Tricks
+I train the network without center_loss first. 
+And I use its result to initial the center location. 
+You can learn more from `update_center.m`.
---
+++ train_id_net_mnist.m
@@ -0,0 +1,42 @@
+function train_id_net_mnist(varargin)
+% -------------------------------------------------------------------------
+% Part 4.1: prepare the data
+% -------------------------------------------------------------------------
+
+% Load character dataset
+imdb = load('./data/mnist/imdb.mat') ;
+set = imdb.images.set;
+imdb.images.set(set==3) = 2;
+% -------------------------------------------------------------------------
+% Part 4.2: initialize a CNN architecture
+% -------------------------------------------------------------------------
+net = LeNets_plusplus();
+net.conserveMemory = true;
+net.meta.normalization.averageImage = imdb.images.data_mean;
+net = update_center(net,imdb);
+% -------------------------------------------------------------------------
+% Part 4.3: train and evaluate the CNN
+% -------------------------------------------------------------------------
+opts.train.averageImage = net.meta.normalization.averageImage;
+opts.train.batchSize = 128;
+opts.train.continue = true; 
+opts.train.gpus = 1;
+opts.train.prefetch = false ;
+opts.train.expDir = './data/LeNets_plusplus_center' ;
+opts.train.derOutputs = {'objective', 1,'objective_center', 0.1} ;
+opts.train.learningRate = [0.001*ones(1,20)] ;
+opts.train.weightDecay = 0.0005;
+opts.train.numEpochs = numel(opts.train.learningRate) ;
+[opts, ~] = vl_argparse(opts.train, varargin) ;
+
+% Call training function in MatConvNet
+[net,info] = cnn_train_dag(net, imdb, @getBatch,opts) ;
+
+% --------------------------------------------------------------------
+function inputs = getBatch(imdb, batch,opts)
+% --------------------------------------------------------------------
+im = imdb.images.data(:,:,batch) ; 
+labels = imdb.images.labels(:,batch) ;
+%im = bsxfun(@minus,im,opts.averageImage);
+im = reshape(im,28,28,1,[]);
+inputs = {'data',gpuArray(im),'label',labels};
